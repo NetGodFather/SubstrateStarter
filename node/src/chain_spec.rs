1e8a2216a6843473bb09bfa126bbbe86b3d3f9d2
ShadowCat <shadowcat@126.com>
2021-02-17 18:04:57 +0800

    chain_spec

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -78,6 +78,61 @@ pub fn development_config() -> Result<ChainSpec, String> {
 	))
 }
 
+pub fn tenglong_testnet_config() -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or("Development wasm binary not available".to_string())?;
+	
+	Ok(ChainSpec::from_genesis(
+		// Name 链的名称，
+		"TengLong Testnet",
+		// ID 链的ID
+		"tenglong_testnet",
+		// 链的类型，支持：Development 开发；Local 本地；Live 公开(包括测试网和主网)；custom 自定义
+		ChainType::Local,
+		// 构造函数
+		move || testnet_genesis(
+			// WASM 文件
+			wasm_binary,
+			// Initial PoA authorities
+			// 初始化 POA 的生产节点账号
+			vec![
+				authority_keys_from_seed("Alice"),
+				authority_keys_from_seed("Bob"),
+			],
+			// Sudo 账号
+			// Sudo account
+			get_account_id_from_seed::<sr25519::Public>("Alice"),
+			// 预先配置的账号，
+			// Pre-funded accounts
+			vec![
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				get_account_id_from_seed::<sr25519::Public>("Bob"),
+				get_account_id_from_seed::<sr25519::Public>("Charlie"),
+				get_account_id_from_seed::<sr25519::Public>("Dave"),
+				get_account_id_from_seed::<sr25519::Public>("Eve"),
+				get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+				get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+				get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+				get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+				get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+			],
+			true,
+		),
+		//
+		// Bootnodes
+		vec![],
+		// Telemetry
+		None,
+		// Protocol ID
+		None,
+		// Properties
+		None,
+		// Extensions
+		None,
+	))
+}
+
 pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or("Development wasm binary not available".to_string())?;
 
@@ -135,25 +190,35 @@ fn testnet_genesis(
 	enable_println: bool,
 ) -> GenesisConfig {
 	GenesisConfig {
+		// 系统模块的初始化
 		frame_system: Some(SystemConfig {
 			// Add Wasm runtime to storage.
+			// 将 WASM 运行时添加到存储中
 			code: wasm_binary.to_vec(),
 			changes_trie_config: Default::default(),
 		}),
+		// 账户余额模块的初始化
 		pallet_balances: Some(BalancesConfig {
+			// 为预制账号设定初始的账户余额
 			// Configure endowed accounts with initial balance of 1 << 60.
 			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
 		}),
+		// aura（POA）区块生产模块的初始化
 		pallet_aura: Some(AuraConfig {
+			// 为 POA 初始化 uthorities 集合
 			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
 		}),
+		// grandpa 模块的初始化
 		pallet_grandpa: Some(GrandpaConfig {
+			// 为 POA 初始化 GRANDPA 账号积累
 			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
 		}),
 		pallet_sudo: Some(SudoConfig {
 			// Assign network admin rights.
+			// 设置 sudo 的 Root 账号
 			key: root_key,
 		}),
+		// 配置 !ink 合约模块的的初始化
 		pallet_contracts: Some(ContractsConfig {
             current_schedule: pallet_contracts::Schedule {
                     enable_println,
