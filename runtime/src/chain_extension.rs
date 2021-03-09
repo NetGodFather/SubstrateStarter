195cf0224d59b63af9e8509cf4a84a227d233ef6
ShadowCat <shadowcat@126.com>
2021-03-09 12:03:06 +0800

    chain_extension update

--- runtime/src/chain_extension.rs
+++ runtime/src/chain_extension.rs
@@ -22,51 +22,43 @@ impl<C: Config> ChainExtension<C> for FetchRandomExtension {
         where
             <E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
     {
-
         match func_id {
             1101 => {
-                // 输出
+                debug::info!("run 1001");
                 let mut env = env.buf_in_buf_out();
-                // 调用 runtime 引入的 pallet：RandomnessCollectiveFlip 生成随机数
                 let random_seed: [u8; 32] = super::RandomnessCollectiveFlip::random_seed().0;
-                // 将生成的随机数进行 encode
+                debug::info!("random_seed: {:?}", random_seed);
                 let random_slice = random_seed.encode();
+                debug::info!("random_slice: {:?}", random_slice);
+                
                 env.write(&random_slice, false, None)
                     .map_err(|_| DispatchError::Other("ChainExtension failed to call random"))?;
             }
             1102 => { // 调用 POE 的创建存证接口，无返回
                 debug::info!("run 1002");
 				let mut env = env.buf_in_buf_out();
-				let caller = env.ext().caller().clone(); // 调用这账户
+				let caller = env.ext().caller().clone();
 				debug::info!("caller: {:?}", caller);
-				let address = env.ext().address(); // 合约账户
-				debug::info!("address: {:?}", address);
-				let in_len = env.in_len();
+				let address = env.ext().address(); // contract
+				debug::info!("contract address: {:?}", address);
+				let in_len = env.in_len(); // Number of passed parameters
 				debug::info!("in_len: {}", in_len);
-
-				let input = env.read(4)?;
-				debug::info!("input with len: {}, {}, {}, {}", input[0], input[1], input[2], input[3]);
-
+				let input = env.read(1)?; // Read n incoming parameters
+				debug::info!("input with len: {}", input[0]);
+                
 				let input: Vec<u8> = env.read_as()?;
-				debug::info!("input: {:?}", input);
-
 				let who = to_account_id(caller.as_ref());
 				super::PoeModule::do_create_claim(who, input)?;
             }
             1103 => { // 调用 Kitties 模块，创建一个 Kitty
                 debug::info!("run 1103");
                 let mut env = env.buf_in_buf_out();
-                
-                let caller = env.ext().caller().clone(); // 调用这账户
-				debug::info!("caller: {:?}", caller);
-				let address = env.ext().address(); // 合约账户
-				debug::info!("address: {:?}", address);
-				let in_len = env.in_len();
-				debug::info!("in_len: {}", in_len);
+                let caller = env.ext().caller().clone();
+				let who = to_account_id( caller.as_ref() );
 
-				let who = to_account_id(caller.as_ref());
                 let kitty_id = super::KittiesModule::create_kitty(&who)?;
-                env.write(&kitty_id.to_le_bytes(), false, None)
+                let kitty_id_encoded = kitty_id.encode();
+                env.write(&kitty_id_encoded, false, None)
                     .map_err(|_| DispatchError::Other("ChainExtension failed to call create_kitty"))?;
             }
             _ => {
